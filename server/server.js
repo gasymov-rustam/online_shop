d9a2bf4aa20a6933dc75af45a0780613da8a2e6b
Gasymov Rustam <rustam.gasymov@devalore.com>
2022-09-15 07:28:11 +0300

    add db and models, create tables

---
+++ server/server.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const express = require("express");
+const sequelize = require("./db");
+const models = require("./models");
+const cors = require("cors");
+
+const PORT = process.env.PORT ?? 5001;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("");
+
+const start = async () => {
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync();
+
+    app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
