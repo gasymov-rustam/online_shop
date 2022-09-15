8cf0c9887a48ed71e32ac9d45ea2fa81274cdf22
Gasymov Rustam <rustam.gasymov@devalore.com>
2022-09-15 08:03:00 +0300

    add base of controllers and class ApiError

---
+++ server/controllers/index.js
@@ -0,0 +1,11 @@
+const userController = require("./userControllers");
+const deviceController = require("./deviceController");
+const typeController = require("./typeController");
+const brandController = require("./barndController");
+
+module.exports = {
+  userController,
+  brandController,
+  deviceController,
+  typeController,
+};
